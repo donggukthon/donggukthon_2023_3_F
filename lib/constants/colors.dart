a61f97e804d6025035aee555363736d134ac04ff
StopSoo <jpy1030@naver.com>
2023-12-18 02:27:49 +0900

    <feat>: font 구성

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//메인 컬러
+const mainColor = Color(0xFFF5E9CF);
+
+//폰트 컬러
+const titleColor = Color(0xFF312E26);
+const fontColor = Color(0xFFA6A6A6);
+
+//버튼 컬러
+const redButtonColor = Color(0xFFED3333);
+const goldButtonColor = Color(0xFFEEBD53);
+const iconButtonColor = Color(0xFF888888);
+
+const whiteColor = Color(0xFFFFFF);
