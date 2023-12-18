02b90a4c246ef21212fdbeb102deced3ebd0c062
hyeyeonismm <hyeyeonismm@gmail.com>
2023-12-18 13:49:19 +0900

    <feat>: 기부내역조회, 프로필화면 추가

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -6,19 +6,104 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String userName = '김혜연';
+    String userEmail = 'hyeyeonismm@gmail.com';
+
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: Center(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(80),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text('profile'),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('profile'),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: Text(
+                  '로그인 계정',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.44999998807907104),
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Container(
+                height: 105,
+                decoration: ShapeDecoration(
+                  color: Colors.white.withOpacity(0.6000000238418579),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(31),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/kakao_icon.png'),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                            child: Text(
+                              '$userName님',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Text(
+                              userEmail,
+                              style: TextStyle(
+                                color: Colors.black
+                                    .withOpacity(0.44999998807907104),
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              //계좌정보 수정
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '계좌 정보 수정',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.44999998807907104),
+                        fontSize: 16,
+                      ),
+                    ),
+                    Container(
+                      height: 290,
+                      decoration: ShapeDecoration(
+                        color: Colors.white.withOpacity(0.6000000238418579),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(31),
+                        ),
+                      ),
+                      child: const Text(
+                        '예금주명',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
