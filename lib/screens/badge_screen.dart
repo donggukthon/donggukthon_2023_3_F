87cc837fdac8b14ecd9c09ecd0d854deb6f60d1f
hyeyeonismm <hyeyeonismm@gmail.com>
2023-12-18 13:43:54 +0900

    <feat>: 뱃지 기능 추가

--- lib/screens/badge_screen.dart
+++ lib/screens/badge_screen.dart
@@ -1,3 +1,5 @@
+import 'package:boonglunteer_view/constants/colors.dart';
+import 'package:boonglunteer_view/widgets/charity_widget.dart';
 import 'package:boonglunteer_view/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
@@ -6,22 +8,59 @@ class BadgeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(80),
-          child: Column(
-            children: [
-              const Text('badge'),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('badge'),
+    return const Scaffold(
+      appBar: CustomAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 35),
+              child: Text(
+                'Badge',
+                style: TextStyle(
+                  color: titleColor,
+                  fontSize: 28,
+                  fontFamily: 'titleFont',
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ],
-          ),
+            ),
+            Text('지금까지 기부한 붕어빵을 통해 획득한 뱃지예요!',
+                style: TextStyle(
+                  color: fontColor,
+                  fontSize: 18,
+                )),
+            Text('각각의 뱃지를 터치하면 상세한 설명을 확인할 수 있어요.',
+                style: TextStyle(
+                  color: fontColor,
+                  fontSize: 18,
+                )),
+            Text('앞으로 또 어떤 붕어빵을 획득할 수 있을까요?',
+                style: TextStyle(
+                  color: fontColor,
+                  fontSize: 18,
+                )),
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 45, 30, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  CharityWidget(charityName: '어린이'),
+                  CharityWidget(charityName: '독거노인'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 15, 30, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  CharityWidget(charityName: '난민'),
+                  CharityWidget(charityName: '저소득층'),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
