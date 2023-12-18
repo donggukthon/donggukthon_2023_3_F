b86f1b2eeedce3ecb71eb0400c0b70e39eb28a43
hyeyeonismm <hyeyeonismm@gmail.com>
2023-12-18 14:46:40 +0900

    <fix>: 폰트 색상 수정

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,11 +5,11 @@ const mainColor = Color(0xFFF5E9CF);
 
 //폰트 컬러
 const titleColor = Color(0xFF312E26);
-const fontColor = Color(0xFFA6A6A6);
+const fontColor = Color(0xFF4A453A);
 
 //버튼 컬러
 const redButtonColor = Color(0xFFED3333);
 const goldButtonColor = Color(0xFFEEBD53);
 const iconButtonColor = Color(0xFF888888);
 
-const whiteColor = Color(0xFFFFFF);
+const whiteColor = Color(0x00ffffff);
