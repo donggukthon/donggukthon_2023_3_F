929cc7189888a161d722251b0e1754e70b154fb5
hyeyeonismm <hyeyeonismm@gmail.com>
2023-12-18 22:21:43 +0900

    <feat>: 모바일 뷰 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {createGlobalStyle} from "styled-components";
+import basicFont from "./assets/fonts/NanumSquareRoundR.ttf";
+import MainPage from "./pages/MainPage";
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'basicFont';
+  src: local('basicFont'), url(${basicFont}) format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+
+ body {
+  font-family: 'basicFont';
+  background-color: black;
+  letter-spacing: -0.1px;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+.layout{
+    max-width: 390px;
+    height: 720px;
+    background-color: #F5E9CF;
+    margin: 0 auto ;
+    overflow-y: scroll;
+    overflow-x: hidden;
+   
+}
+
+.layout::-webkit-scrollbar {
+  width: 0;
+}
+
+.layout::-webkit-scrollbar-thumb {
+}
+
+`;
+
 function App() {
   return (
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
+    <BrowserRouter>
+      <GlobalStyle />
+      <div className="layout">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/MainPage.js
@@ -0,0 +1,9 @@
+function MainPage() {
+  return (
+    <>
+      <p>메인페이지</p>
+    </>
+  );
+}
+
+export default MainPage;
