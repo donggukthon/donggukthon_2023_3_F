5b696f95899b82d4707f3f74074310707844bc79
hyeyeonismm <hyeyeonismm@gmail.com>
2023-12-19 21:14:10 +0900

    <fix>:api연결

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -1,21 +1,30 @@
 import {GoogleLogin} from "@react-oauth/google";
 import {GoogleOAuthProvider} from "@react-oauth/google";
+import instance from "../api/axios";
 
 const GoogleLoginButton = () => {
   const clientId = "317787650438-melnlpu3vcr53d2l4oc2e22f7jg8ra98.apps.googleusercontent.com";
+
+  const handleLoginSuccess = async (res) => {
+    try {
+      const response = await instance.post("/accounts/google/callback", {
+        token: res.credential,
+      });
+
+      console.log(response.data); // 백엔드로부터의 응답 처리
+    } catch (err) {
+      console.error("Error:", err); // 에러 처리
+    }
+  };
+
+  const handleLoginFailure = (err) => {
+    console.error("Login Failed:", err);
+  };
+
   return (
-    <>
-      <GoogleOAuthProvider clientId={clientId}>
-        <GoogleLogin
-          onSuccess={(res) => {
-            console.log(res);
-          }}
-          onFailure={(err) => {
-            console.log(err);
-          }}
-        />
-      </GoogleOAuthProvider>
-    </>
+    <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin onSuccess={handleLoginSuccess} onFailure={handleLoginFailure} />
+    </GoogleOAuthProvider>
   );
 };
 
