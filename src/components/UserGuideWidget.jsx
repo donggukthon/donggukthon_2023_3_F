4513575eb43e23fdd3ee76e94e946bd0f1427673
hyeyeonismm <hyeyeonismm@gmail.com>
2023-12-19 15:56:04 +0900

    <fix>: 충돌 해결

--- src/components/UserGuideWidget.jsx
+++ src/components/UserGuideWidget.jsx
@@ -1,85 +1,90 @@
 import Carousel from 'react-material-ui-carousel';
-import styled from "styled-components";
+import styled from 'styled-components';
 import fishbread from '../assets/images/santa_fishbread_brown.png';
 
 function UserGuideWidget() {
-  const items = [
-    {
-      content: "기부가 하고 싶은데 방법을 모르겠고,,<br>기부를 하는 과정이 너무 번거로우셨나요?<br><strong style='color:red'>♥사랑의 붕어빵♥</strong>을 통해 여러분의 마음을 쉽고 간단하게 전달해보세요 !",
-      image: fishbread
-    },
-    {
-      content: "Q. <strong style='color:red'>♥사랑의 붕어빵♥</strong>은 어떤 서비스 인가요?<br><br>기부 금액이 쌓임에 따라 붕어빵이 만들어지게 됩니다. 3000원이 모이면 붕어빵이 완성되고 여러분이 원하는 단체에 기부를 할 수 있습니다.",
-      image: null
-    },
-    {
-      content: "Q. <strong style='color:red'>♥사랑의 붕어빵♥</strong>의 특징은 무엇인가요?<br>여러분이 기부를 하실때마다 랜덤의 붕어빵이 만들어지게 됩니다. 기부 금액에 따라 붕어빵은 점점 성장하게 되고, 성장한 붕어빵을 기부하게 되면 배지로 남게 됩니다.<br><br>여러분의 마음을 전달하고 아기자기한 붕어빵을 모아보세요 ! ",
-      image: null
-    },
-    {
-      content: "Q. 꼭 3000원을 모아야 기부가 가능한가요?<br><br>아닙니다. 기부는 3000원보다 적어도 많아도 가능합니다! 하지만 <strong>3000원이 모여야만 배지를 얻을 수 있다는 점</strong> 기억해주세요 !",
-      image: null
-    },
-    {
-      content: "Q. 구체적인 기부 방식은 어떻게 되나요?<br><br>사랑의 붕어빵은 여러분이 선택한 요일 22시에 계좌의 자투리 금액 (1000원 이하)의 금액이 자동으로 모이게 됩니다.",
-      image: null
-    },
-    {
-      content: "Q. 어떤 단체에 기부가 되나요?<br><br>기부단체는 여러분이 직접 선택가능합니다. 붕어빵을 전달할 때 난민, 독거 노인, 저소득층, 어린이 중 선택할 수 있게 됩니다.",
-      image: null
-    },
-  ];
+	const items = [
+		{
+			content:
+				"기부가 하고 싶은데 방법을 모르겠고,,<br>기부를 하는 과정이 너무 번거로우셨나요?<br><strong style='color:red'>♥사랑의 붕어빵♥</strong>을 통해 여러분의 마음을 쉽고 간단하게 전달해보세요 !",
+			image: fishbread,
+		},
+		{
+			content:
+				"Q. <strong style='color:red'>♥사랑의 붕어빵♥</strong>은 어떤 서비스 인가요?<br><br>기부 금액이 쌓임에 따라 붕어빵이 만들어지게 됩니다. 3000원이 모이면 붕어빵이 완성되고 여러분이 원하는 단체에 기부를 할 수 있습니다.",
+			image: null,
+		},
+		{
+			content:
+				"Q. <strong style='color:red'>♥사랑의 붕어빵♥</strong>의 특징은 무엇인가요?<br>여러분이 기부를 하실때마다 랜덤의 붕어빵이 만들어지게 됩니다. 기부 금액에 따라 붕어빵은 점점 성장하게 되고, 성장한 붕어빵을 기부하게 되면 배지로 남게 됩니다.<br><br>여러분의 마음을 전달하고 아기자기한 붕어빵을 모아보세요 ! ",
+			image: null,
+		},
+		{
+			content:
+				'Q. 꼭 3000원을 모아야 기부가 가능한가요?<br><br>아닙니다. 기부는 3000원보다 적어도 많아도 가능합니다! 하지만 <strong>3000원이 모여야만 배지를 얻을 수 있다는 점</strong> 기억해주세요 !',
+			image: null,
+		},
+		{
+			content:
+				'Q. 구체적인 기부 방식은 어떻게 되나요?<br><br>사랑의 붕어빵은 여러분이 선택한 요일 22시에 계좌의 자투리 금액 (1000원 이하)의 금액이 자동으로 모이게 됩니다.',
+			image: null,
+		},
+		{
+			content:
+				'Q. 어떤 단체에 기부가 되나요?<br><br>기부단체는 여러분이 직접 선택가능합니다. 붕어빵을 전달할 때 난민, 독거 노인, 저소득층, 어린이 중 선택할 수 있게 됩니다.',
+			image: null,
+		},
+	];
 
-  return (
-    <CarouselContainer>
-      <Carousel
-        autoPlay={false}
-        animation='slide'
-        timeout={500}
-        navButtonsAlwaysVisible={false}
-        navButtonsAlwaysInvisible={false}
-      >
-        {items.map((item, i) => (
-          <CarouselItem key={i}>
-            <ContentContainer>
-              <TextContainer dangerouslySetInnerHTML={{ __html: item.content }} />
-              {item.image && <ImageContainer src={item.image} alt="Fishbread" />}
-            </ContentContainer>
-          </CarouselItem>
-        ))}
-      </Carousel>
-    </CarouselContainer>
-  );
+	return (
+		<CarouselContainer>
+			<Carousel
+				autoPlay={false}
+				animation='slide'
+				timeout={500}
+				navButtonsAlwaysVisible={false}
+				navButtonsAlwaysInvisible={false}>
+				{items.map((item, i) => (
+					<CarouselItem key={i}>
+						<ContentContainer>
+							<TextContainer dangerouslySetInnerHTML={{ __html: item.content }} />
+							{item.image && <ImageContainer src={item.image} alt='Fishbread' />}
+						</ContentContainer>
+					</CarouselItem>
+				))}
+			</Carousel>
+		</CarouselContainer>
+	);
 }
 
 const CarouselContainer = styled.div`
-  overflow: hidden;
-  width: 100%;
-  height: 100%;
+	overflow: hidden;
+	width: 100%;
+	height: 100%;
 `;
 
 const CarouselItem = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  font-size: 15px;
-  font-family: 'lineFont';
+	width: 100%;
+	height: 100%;
+	display: flex;
+	font-size: 15px;
+	font-family: 'lineFont';
 `;
 
 const ContentContainer = styled.div`
-  text-align: center;
-  max-width: 100%;
-  margin: 0 auto;
+	text-align: center;
+	max-width: 100%;
+	margin: 0 auto;
 `;
 
 const TextContainer = styled.div`
-  margin-bottom: 10px; 
+	margin-bottom: 10px;
 `;
 
 const ImageContainer = styled.img`
-  width: 110px;
-  /* max-width: 100%; */
-  height: auto;
+	width: 110px;
+	/* max-width: 100%; */
+	height: auto;
 `;
 
 export default UserGuideWidget;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import styled from "styled-components";
-import presentRoot from "../assets/images/present.png";
-import UserGuideWidget from '../components/UserGuideWidget.jsx';
+import React from 'react';
+import styled from 'styled-components';
+import presentRoot from '../assets/images/present.png';
+import UserGuideWidget from '../components/UserGuideWidget';
 
 function MainPage() {
-  return (
-    <>
-      <ImagePart>
-        <img src={presentRoot} width="180px" padding="0" />
-      </ImagePart>
-      <TextPart>
-        <text>
-          Create Bread
-          <br />
-          and Donate!
-        </text>
-      </TextPart>
-      <WhiteBox>
-        <UserGuideWidgetContainer>
-          <UserGuideWidget />
-        </UserGuideWidgetContainer>
-      </WhiteBox>
-      <StartButton>시작하기</StartButton>
-    </>
-  );
+	return (
+		<>
+			<ImagePart>
+				<img src={presentRoot} width='180px' />
+			</ImagePart>
+			<TextPart>
+				<text>
+					Create Bread
+					<br />
+					and Donate!
+				</text>
+			</TextPart>
+			<WhiteBox>
+				<UserGuideWidgetContainer>
+					<UserGuideWidget />
+				</UserGuideWidgetContainer>
+			</WhiteBox>
+			<StartButton>시작하기</StartButton>
+		</>
+	);
 }
 
 const ImagePart = styled.div`
@@ -45,48 +45,48 @@ const TextPart = styled.div`
 	display: flex;
 	justify-content: center;
 
-  text {
-    font-family: "lineFont";
-    font-size: 30px;
-    font-weight: 700;
-  }
+	text {
+		font-family: 'lineFont';
+		font-size: 30px;
+		font-weight: 700;
+	}
 `;
 
 const WhiteBox = styled.div`
-  width: 250px;
-  height: 250px;
-  background-color: white;
-  opacity: 60%;
-  border-radius: 15px;
-  margin: 20px auto;
-  position: relative;
-  display: flex;
-  overflow-x: auto;
-  padding: 20px;
+	width: 250px;
+	height: 250px;
+	background-color: white;
+	opacity: 60%;
+	border-radius: 15px;
+	margin: 20px auto;
+	position: relative;
+	display: flex;
+	overflow-x: auto;
+	padding: 20px;
 `;
 
 // UserGuideWidget의 너비, 높이 조정
 const UserGuideWidgetContainer = styled.div`
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 `;
 
 const StartButton = styled.button`
-  width: 250px;
-  height: 45px;
-  background: var(--button-bg-color, #ed3333);
-  border: none;
-  border-radius: 15px 15px 15px 15px;
-  font-family: "Godo", sans-serif;
-  font-size: 17px;
-  color: white;
-  margin: 0 auto;
-  padding: auto 0 auto 0;
-  box-shadow: 3px 3px 3px 0px gray;
-  opacity: 90%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	width: 250px;
+	height: 45px;
+	background: var(--button-bg-color, #ed3333);
+	border: none;
+	border-radius: 15px 15px 15px 15px;
+	font-family: 'Godo', sans-serif;
+	font-size: 17px;
+	color: white;
+	margin: 0 auto;
+	padding: auto 0 auto 0;
+	box-shadow: 3px 3px 3px 0px gray;
+	opacity: 90%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
 	cursor: pointer;
 	&:active,
