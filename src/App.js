27368799c2aec1e4d20b8198419858ababb7ccff
hyeyeonismm <hyeyeonismm@gmail.com>
2023-12-19 16:16:38 +0900

    <feat>: 버튼 클릭 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
-import basicFont from './assets/fonts/NanumSquareRoundR.ttf';
-import titleFont from './assets/fonts/Jalnan2TTF.ttf';
-import MainPage from './pages/MainPage';
-import HomePage from './pages/HomePage';
-import CharityPage from './pages/CharityPage';
-import BadgePage from './pages/BadgePage';
-import MyPage from './pages/MyPage';
-import HistoryPage from './pages/HistoryPage';
-import DetailPage from './pages/DetailPage';
-import LoginPage from './pages/LoginPage';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {createGlobalStyle} from "styled-components";
+import basicFont from "./assets/fonts/NanumSquareRoundR.ttf";
+import titleFont from "./assets/fonts/Jalnan2TTF.ttf";
+import lineFont from "./assets/fonts/RampartOne.ttf";
+import MainPage from "./pages/MainPage";
+import HomePage from "./pages/HomePage";
+import CharityPage from "./pages/CharityPage";
+import BadgePage from "./pages/BadgePage";
+import MyPage from "./pages/MyPage";
+import HistoryPage from "./pages/HistoryPage";
+import DetailPage from "./pages/DetailPage";
+import LoginPage from "./pages/LoginPage";
 
 const GlobalStyle = createGlobalStyle`
 @font-face {
@@ -61,23 +62,23 @@ body {
 `;
 
 function App() {
-	return (
-		<BrowserRouter>
-			<GlobalStyle />
-			<div className='layout'>
-				<Routes>
-					<Route path='/' element={<MainPage />} />
-					<Route path='/login' element={<LoginPage />} />
-					<Route path='/home' element={<HomePage />} />
-					<Route path='/charity' element={<CharityPage />} />
-					<Route path='/badge' element={<BadgePage />} />
-					<Route path='/profile' element={<MyPage />} />
-					<Route path='/history' element={<HistoryPage />} />
-					<Route path='/detail' element={<DetailPage />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <div className="layout">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/charity" element={<CharityPage />} />
+          <Route path="/badge" element={<BadgePage />} />
+          <Route path="/profile" element={<MyPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/detail" element={<DetailPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,108 +1,111 @@
-import React from 'react';
-import styled from 'styled-components';
-import presentRoot from '../assets/images/present.png';
-import UserGuideWidget from '../components/UserGuideWidget';
+import React from "react";
+import {useNavigate, useLocation} from "react-router-dom";
+import styled from "styled-components";
+import presentRoot from "../assets/images/present.png";
+import UserGuideWidget from "../components/UserGuideWidget";
 
 function MainPage() {
-	return (
-		<>
-			<ImagePart>
-				<img src={presentRoot} width='180px' />
-			</ImagePart>
-			<TextPart>
-				<text>
-					Create Bread
-					<br />
-					and Donate!
-				</text>
-			</TextPart>
-			<WhiteBox>
-				<UserGuideWidgetContainer>
-					<UserGuideWidget />
-				</UserGuideWidgetContainer>
-			</WhiteBox>
-			<StartButton>시작하기</StartButton>
-		</>
-	);
+  const navigate = useNavigate();
+  const onClickStart = () => {
+    navigate("/login");
+  };
+  return (
+    <>
+      <ImagePart>
+        <img src={presentRoot} width="180px" />
+      </ImagePart>
+      <TextPart>
+        <text>
+          Create Bread
+          <br />
+          and Donate!
+        </text>
+      </TextPart>
+      <WhiteBox>
+        <UserGuideWidgetContainer>
+          <UserGuideWidget />
+        </UserGuideWidgetContainer>
+      </WhiteBox>
+      <StartButton onClick={onClickStart}>시작하기</StartButton>
+    </>
+  );
 }
 
 const ImagePart = styled.div`
-	background-color: none;
-	border-radius: 100px;
-	margin: 30px 0 0 0;
-	position: relative;
-	display: flex;
-	justify-content: center;
+  background-color: none;
+  border-radius: 100px;
+  margin: 30px 0 0 0;
+  position: relative;
+  display: flex;
+  justify-content: center;
 
-	img {
-		margin: 0 auto 0 auto;
-	}
+  img {
+    margin: 0 auto 0 auto;
+  }
 `;
 
 const TextPart = styled.div`
-	border: none;
-	position: relative;
-	display: flex;
-	justify-content: center;
+  border: none;
+  position: relative;
+  display: flex;
+  justify-content: center;
 
-	text {
-		font-family: 'lineFont';
-		font-size: 30px;
-		font-weight: 700;
-	}
+  text {
+    font-family: "lineFont";
+    font-size: 30px;
+    font-weight: 700;
+  }
 `;
 
 const WhiteBox = styled.div`
-	width: 250px;
-	height: 250px;
-	background-color: white;
-	opacity: 60%;
-	border-radius: 15px;
-	margin: 20px auto;
-	position: relative;
-	display: flex;
-	overflow-x: auto;
-	padding: 20px;
+  width: 250px;
+  height: 250px;
+  background-color: white;
+  opacity: 60%;
+  border-radius: 15px;
+  margin: 20px auto;
+  position: relative;
+  display: flex;
+  overflow-x: auto;
+  padding: 20px;
 `;
 
 // UserGuideWidget의 너비, 높이 조정
 const UserGuideWidgetContainer = styled.div`
-	width: 100%;
-	height: 100%;
+  width: 100%;
+  height: 100%;
 `;
 
 const StartButton = styled.button`
-	width: 250px;
-	height: 45px;
-	background: var(--button-bg-color, #ed3333);
-	border: none;
-	border-radius: 15px 15px 15px 15px;
-	font-family: 'Godo', sans-serif;
-	font-size: 17px;
-	color: white;
-	margin: 0 auto;
-	padding: auto 0 auto 0;
-	box-shadow: 3px 3px 3px 0px gray;
-	opacity: 90%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  width: 250px;
+  height: 45px;
+  background: var(--button-bg-color, #ed3333);
+  border: none;
+  border-radius: 15px 15px 15px 15px;
+  font-family: "Godo", sans-serif;
+  font-size: 17px;
+  color: white;
+  margin: 0 auto;
+  padding: auto 0 auto 0;
+  box-shadow: 3px 3px 3px 0px gray;
+  opacity: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-	cursor: pointer;
-	&:active,
-	&:hover {
-		opacity: 100%;
-	}
+  cursor: pointer;
+  &:active,
+  &:hover {
+    opacity: 100%;
+  }
 
-	// 고도체
-	@font-face {
-		font-family: 'Godo';
-		font-style: normal;
-		font-weight: 700;
-		src:
-			url('//cdn.jsdelivr.net/korean-webfonts/1/corps/godo/Godo/GodoB.woff2') format('woff2'),
-			url('//cdn.jsdelivr.net/korean-webfonts/1/corps/godo/Godo/GodoB.woff') format('woff');
-	}
+  // 고도체
+  @font-face {
+    font-family: "Godo";
+    font-style: normal;
+    font-weight: 700;
+    src: url("//cdn.jsdelivr.net/korean-webfonts/1/corps/godo/Godo/GodoB.woff2") format("woff2"), url("//cdn.jsdelivr.net/korean-webfonts/1/corps/godo/Godo/GodoB.woff") format("woff");
+  }
 `;
 
 export default MainPage;
