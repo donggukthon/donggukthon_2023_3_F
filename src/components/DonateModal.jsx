1eb459e1b6cedea5b838b22bea888ed6747f205d
StopSoo <jpy1030@naver.com>
2023-12-20 15:33:40 +0900

    <feat>: 메인 화면 기부하기 모달 버튼 기능 구현

--- src/components/DonateModal.jsx
+++ src/components/DonateModal.jsx
@@ -1,12 +1,91 @@
 import React, {useState, useEffect} from "react";
 import Close from "@mui/icons-material/CloseRounded";
 import {styled, Box, Grid, Button, Img, Modal} from "@mui/material";
+import fishbread from "../assets/images/badge1.png";
 
 function DonateModal({isOpen, onClose}) {
+  const [isYes, setIsYes] = useState(false);
+
+  const IsYesButtonClicked = () => {
+    setIsYes(true);
+  };
+
   return (
     <>
       <Modal open={isOpen} onClose={onClose}>
-        <ModalGrid>google</ModalGrid>
+        <ModalGrid>
+          <CloseButton
+            onClick={onClose}
+          />
+          <CommentPart>
+            <text>{!isYes ? "기부하시겠습니까 ?" : "배지를 획득하셨습니다."}</text>
+            <ImagePart>
+              <img src={fishbread} alt='logo' width='60px'/>
+            </ImagePart>
+            {!isYes 
+            // 예 버튼을 클릭하지 않았을 떄
+              ? <ButtonPart>
+              <Button
+                onClick={IsYesButtonClicked}
+                sx={{
+                  margin: "20px 0px",
+                  background: "#ED3333",
+                  width: "130px",
+                  height: "50px",
+                  padding: "12px 20px",
+                  color: "white",
+                  borderRadius: "16px",
+                  fontSize: "15px",
+                  "&:hover": {
+                    background: "rgba(237, 51, 51, 0.50)",
+                  },
+                }}
+              >
+                예
+              </Button>
+              <Button
+                onClick={onClose}
+                sx={{
+                  margin: "20px 0px",
+                  background: "#ED3333",
+                  width: "130px",
+                  height: "50px",
+                  padding: "12px 20px",
+                  color: "white",
+                  borderRadius: "16px",
+                  fontSize: "15px",
+                  "&:hover": {
+                    background: "rgba(237, 51, 51, 0.50)",
+                  },
+                }}
+              >
+                아니오
+              </Button>
+            </ButtonPart>
+            // 예 버튼을 클릭했을 때
+              : <ButtonPart>
+              <Button
+                onClick={onClose}
+                sx={{
+                  margin: "20px 0px",
+                  background: "#ED3333",
+                  width: "260px",
+                  height: "50px",
+                  padding: "12px 20px",
+                  color: "white",
+                  borderRadius: "16px",
+                  fontSize: "15px",
+                  "&:hover": {
+                    background: "rgba(237, 51, 51, 0.50)",
+                  },
+                }}
+              >
+                확인
+              </Button>
+            </ButtonPart>
+            }
+          </CommentPart>
+        </ModalGrid>
       </Modal>
     </>
   );
@@ -15,10 +94,10 @@ function DonateModal({isOpen, onClose}) {
 const CloseButton = styled(Close)(() => ({
   display: "flex",
   justifyContent: "flex-end",
-
   cursor: "pointer",
   color: "grey",
   fontSize: 26,
+  margin: "10px 0 20px 0",
   "&:hover": {
     color: "lightgrey",
   },
@@ -38,4 +117,37 @@ const ModalGrid = styled(Grid)(() => ({
   boxShadow: "0 10px 50px rgb(70, 70, 70)",
   padding: "10px 20px 10px 20px",
 }));
+
+const CommentPart = styled(Grid)`
+  border: none;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  text {
+    font-family: "descriptBoldFont";
+    font-size: 20px;
+    font-weight: 700;
+    margin: 10 auto;
+  }
+`;
+
+const ButtonPart = styled(Grid)`
+  border: none;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  text-align: center;
+`;
+
+const ImagePart = styled(Grid)`
+  border: none;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  margin: 10px 0;
+`;
 export default DonateModal;
